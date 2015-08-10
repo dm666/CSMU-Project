a19005b4ae890afad0840a8f3a1a0149ee6314bd
dm666 <medfac666@mail.com>
2015-08-10 12:57:58 +0300

    Add CSMU Project

---
+++ CSMU Knowledge/CSMU Knowledge/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Elysium;
+
+namespace CSMU_Knowledge
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void StartupHandler(object sender, System.Windows.StartupEventArgs e)
+        {
+            this.Apply(Theme.Dark, AccentBrushes.Blue, Brushes.White);
+        }
+    }
+}
